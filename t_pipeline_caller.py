44ec32ee205088afba35789944a9766597693427
Ferit Tunçer <ferit.tuncer@autistici.org>
2016-02-13 22:11:05 +0100

    test update

--- t_pipeline_caller.py
+++ t_pipeline_caller.py
@@ -6,14 +6,14 @@ import unittest
 
 class TestPlusOne(unittest.TestCase):
 
-    def test_main_returns_zero_on_success(self):
-        self.assertEquals(pipeline_caller.tokenTest(), 1)
+	def test_main_returns_zero_on_success(self):
+		self.assertEquals(pipeline_caller.tokenTest(), 1)
 
-    def test_main_returns_zero_on_success2(self):
-        self.assertEquals(pipeline_caller.tokenTest2(), 2)
+	def test_main_returns_zero_on_success2(self):
+		self.assertEquals(pipeline_caller.tokenTest2(), 2)
 
-    def test_general(self):
-	assert_not_equal(pipeline_caller.general_test(), "Invalid token")
+	def test_general(self):
+		assert_not_equal(pipeline_caller.general_test(), "Invalid token")
 
 if __name__ == '__main__':
     unittest.main()
