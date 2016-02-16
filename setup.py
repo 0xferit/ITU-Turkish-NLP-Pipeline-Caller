ed4910bdeb11c4e4d7a70e78b8a40790bdc6daa7
Ferit Tunçer <ferit.tuncer@autistici.org>
2016-02-17 00:54:22 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "ITU Turkish NLP Pipeline Caller",
+    version = "1.0.0",
+    packages = find_packages(),
+    scripts = ['pipeline_caller.py'],
+    author = "Ferit Tunçer",
+    author_email = "ferit.tuncer@autistici.org",
+    description = "A wrapper tool to use ITU Turkish NLP Pipeline API",
+    license = "GNUv2",
+    keywords = "ITU Turkish NLP Pipeline",
+    url = "https://github.com/ferittuncer/ITU-Turkish-NLP-Pipeline-Caller",
+)
