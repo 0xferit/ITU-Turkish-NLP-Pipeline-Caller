5cc85d57aef6348a275f04a6548615903df0983e
Ferit Tunçer <ferit.tuncer@autistici.org>
2016-02-27 17:33:29 +0100

    revising according to new module usage

--- tests_pipeline_caller.py
+++ tests_pipeline_caller.py
@@ -2,23 +2,21 @@
 
 import pipeline_caller
 import unittest
-import nose.tools
-
 
 class Test(unittest.TestCase):
 
     def module_exception_test(self):
         try:
-            caller = pipeline_caller.PipelineCaller()
-            caller.call("pipelineNoisy", "test sentence", "random token")
+            caller = pipeline_caller.PipelineCaller('pipelineNoisy', 'test sentence', 'random token', 'whole')
+            caller.call()
         except:
-            self.fail("Exception thrown")
+            self.fail('Exception thrown')
     
     def tool_exception_test(self):
         try:
-            exec(open("./pipeline_caller.py").read())
+            exec(open('./pipeline_caller.py').read())
         except:
-            self.fail("Exception thrown")
+            self.fail('Exception thrown')
 
 if __name__ == '__main__':
     unittest.main()
