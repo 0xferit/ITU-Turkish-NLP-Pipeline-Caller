71a50e206802034e47dc24ae9a8ab8e271416095
Ferit Tunçer <ferit.tuncer@autistici.org>
2016-02-27 20:48:22 +0100

    environment variable

--- tests_pipeline_caller.py
+++ tests_pipeline_caller.py
@@ -5,20 +5,22 @@ import unittest
 import os
 import re
 
+KATANA = 'Katana\'yı saklarız, dedi Ramiz, ben giderim bahçeye, tüm yeşil erikleri toplar gelirim, sonra da erikleri komşuya götürür, ondan Katana\'yı ele vermemesini isterim.'
+
 class Test(unittest.TestCase):
 
     def module_pipelineNoisy_test(self):
 
         try:
 
-            caller = pipeline_caller.PipelineCaller('pipelineNoisy', 'test sentence', os.environ['pipeline_token'], 'whole')
+            caller = pipeline_caller.PipelineCaller('pipelineNoisy', KATANA, os.environ['pipeline_token'], 'whole')
             
-            r = re.compile(r'(\d+)(\t.+?){7,}')
+            r = re.compile(r'(\d+)(\t.+?){7,}', re.MULTILINE)
             
             response = caller.call()
             
             print(response)
-            assert re.match(r, response) != None
+            assert len(re.findall(r, response)) == 28
         except:
             self.fail('Exception thrown')
     
