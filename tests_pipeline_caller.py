254e1ec5be29ba7e73e33f3feb55b125a96e50ff
Ferit Tunçer <ferit.tuncer@autistici.org>
2016-02-27 20:30:08 +0100

    regex trial

--- tests_pipeline_caller.py
+++ tests_pipeline_caller.py
@@ -3,15 +3,13 @@
 import pipeline_caller
 import unittest
 import os
-from io import StringIO
 
 class Test(unittest.TestCase):
 
     def module_exception_test(self):
-            saved_stdout = sys.stdout
+
         try:
-            out = StringIO()
-            sys.stdout = out
+
             caller = pipeline_caller.PipelineCaller('pipelineNoisy', 'test sentence', os.environ['pipeline_token'], 'whole')
             
             r = re.compile(r'(\d+)(\t.+?){7,}')
