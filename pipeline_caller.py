bc18675ed75835650fafe55f31a4161e21e85f31
Ferit Tunçer <ferit.tuncer@autistici.org>
2016-02-27 17:12:16 +0100

    variable rename

--- pipeline_caller.py
+++ pipeline_caller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 name = "ITU Turkish NLP Pipeline Caller"
-copyright = "Copyright 2015 Ferit Tunçer"
+__copyright = "Copyright 2015 Ferit Tunçer"
 
 license = "GPLv2\n\
 This program is free software; you can redistribute it and/or \
@@ -14,7 +14,7 @@ GNU General Public License for more details. \
 You should have received a copy of the GNU General Public License \
 along with this program.  If not, see <http://www.gnu.org/licenses/>."
 
-author = "Ferit Tunçer"
+__author__ = "Ferit Tunçer"
 email = "ferit.tuncer@autistici.org"
 website = "https://github.com/ferittuncer/ITU-Turkish-NLP-Pipeline-Caller"
 
@@ -30,15 +30,16 @@ import re
 import locale
 
 TOKEN_PATH = "pipeline.token"
+DEFAULT_ENCODING = locale.getpreferredencoding(False)
+DEFAULT_OUTPUT_DIR = "output"
 
 class PipelineCaller:
 
-
-    DEFAULT_ENCODING = locale.getpreferredencoding(False)
-    DEFULT_SENTENCE_SPLIT_DELIMITER_CLASS = "[\.\?:;!]"
-    DEFAULT_OUTPUT_DIR = "output"
     API_URL = "http://tools.nlp.itu.edu.tr/SimpleApi"
     PIPELINE_ENCODING = 'UTF-8'
+
+    DEFULT_SENTENCE_SPLIT_DELIMITER_CLASS = "[\.\?:;!]"
+
 
     def __init__(self, tool='pipelineNoisy', text ='example', token='invalid', processing_type='whole'):
         self.tool = tool
@@ -84,7 +85,7 @@ class PipelineCaller:
 
     def getSentences(self):
         r = re.compile(r'(?<=(?:{}))\s+'.format(DEFULT_SENTENCE_SPLIT_DELIMITER_CLASS))
-        sentences = r.split(full_text)
+        sentences = r.split(text)
         sentence_count = len(sentences)
         if re.match("^\s*$", sentences[sentence_count-1]):
             sentences.pop(sentence_count-1)
@@ -99,10 +100,10 @@ class PipelineCaller:
 def __readInput(path, encoding):
     try:
         with open(path, encoding=encoding) as input_file:
-            full_text = ""
+            text = ""
             for line in input_file:
-                full_text += line
-        return full_text
+                text += line
+        return text
     except:
         raise
 
@@ -129,28 +130,28 @@ def __conditional_info(to_be_printed, quiet):
 
 def __parseArguments():
     #epilog section is free now
-    arg_parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description="ITU Turkish NLP Pipeline Caller v{}\n{} <{}>\n{}".format(version, author, email, website),
     add_help=True)
-    arg_parser.add_argument("filename", help="relative input filepath")
-    arg_parser.add_argument('-p', '--processing-type', dest='processing_type', choices=['word', 'sentence', 'whole'], default='whole', help='Switches processing type, default is whole text at once. Alternatively, word by word or sentence by sentence processing can be selected.')
-    arg_parser.add_argument("-q", "--quiet", dest="quiet", action="store_true", help="no info during process")
-    arg_parser.add_argument("--tool", dest="tool", default="pipelineNoisy", choices=["ner", "morphanalyzer", "isturkish",  "morphgenerator", "tokenizer", "normalize", "deasciifier", "Vowelizer", "DepParserFormal", "DepParserNoisy", "spellcheck", "disambiguator", "pipelineFormal", "pipelineNoisy"], help="Switches pipeline tool which is \"pipelineNoisy\" by default")
-    arg_parser.add_argument("-e", "--encoding", dest="encoding", metavar="E", default=PipelineCaller.DEFAULT_ENCODING, help="force I/O to use given encoding, instead of default locale")
-    arg_parser.add_argument("-o", "--output", metavar="O", dest="output_dir", default=PipelineCaller.DEFAULT_OUTPUT_DIR, help="change output directory, \"{}\" by default".format(PipelineCaller.DEFAULT_OUTPUT_DIR))
-    arg_parser.add_argument('--version', action='version', version='{} {}'.format(name, version), help="version information")
-    arg_parser.add_argument('--license', action='version', version='{}'.format(license), help="license information")
+    parser.add_argument("filename", help="relative input filepath")
+    parser.add_argument('-p', '--processing-type', dest='processing_type', choices=['word', 'sentence', 'whole'], default='whole', help='Switches processing type, default is whole text at once. Alternatively, word by word or sentence by sentence processing can be selected.')
+    parser.add_argument("-q", "--quiet", dest="quiet", action="store_true", help="no info during process")
+    parser.add_argument("--tool", dest="tool", default="pipelineNoisy", choices=["ner", "morphanalyzer", "isturkish",  "morphgenerator", "tokenizer", "normalize", "deasciifier", "Vowelizer", "DepParserFormal", "DepParserNoisy", "spellcheck", "disambiguator", "pipelineFormal", "pipelineNoisy"], help="Switches pipeline tool which is \"pipelineNoisy\" by default")
+    parser.add_argument("-e", "--encoding", dest="encoding", metavar="E", default=DEFAULT_ENCODING, help="force I/O to use given encoding, instead of default locale")
+    parser.add_argument("-o", "--output", metavar="O", dest="output_dir", default=DEFAULT_OUTPUT_DIR, help="change output directory, \"{}\" by default".format(DEFAULT_OUTPUT_DIR))
+    parser.add_argument('--version', action='version', version='{} {}'.format(name, version), help="version information")
+    parser.add_argument('--license', action='version', version='{}'.format(license), help="license information")
 
 
-    return arg_parser.parse_args()
+    return parser.parse_args()
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
         
     args = __parseArguments()
-    full_text = __readInput(args.filename, args.encoding)
+    text = __readInput(args.filename, args.encoding)
     output_path = __getOutputPath(args.output_dir)
     token = __readToken()
     __conditional_info("[INFO] Pipeline tool: {}".format(args.tool), args.quiet)
@@ -158,7 +159,7 @@ def main(args=None):
     __conditional_info("[INFO] Output destination: .{}{}".format(os.sep, output_path), args.quiet)
     start_time = time.time()
 
-    caller = PipelineCaller(args.tool, full_text, token, args.processing_type)
+    caller = PipelineCaller(args.tool, text, token, args.processing_type)
     with open(output_path, 'w', encoding=args.encoding) as output_file:    
         output_file.write("{}\n".format(caller.call()))
     print("[DONE] It took {0} seconds to process whole text.".format(str(time.time()-start_time).split('.')[0]))
