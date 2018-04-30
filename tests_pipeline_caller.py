668a59c75d778d284a14dccbb046a8fd6ce1125f
Ferit Tunçer <ferit@lavabit.com>
2018-04-30 13:45:12 +0300

    Updates test case value

--- tests_pipeline_caller.py
+++ tests_pipeline_caller.py
@@ -61,7 +61,7 @@ class Test(unittest.TestCase):
             response = caller.call()
 
             print(response)
-            assert len(re.findall(r, response)) == 33
+            assert len(re.findall(r, response)) == 29
         except:
             self.fail('Exception thrown')
 
