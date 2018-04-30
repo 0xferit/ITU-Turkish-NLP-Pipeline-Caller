dc58a172c393d2e5ee16da628c6cfba93e5e8229
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2018-04-30 18:18:42 +0300

    Remove bare except blocks, let things crash louder

--- tests_pipeline_caller.py
+++ tests_pipeline_caller.py
@@ -20,67 +20,44 @@ UCDORT = '    Üç   -      dört      kız      başı      göründü      .  
 
 class Test(unittest.TestCase):
     def module_Vowelizer_word_test(self):
+        caller = pipeline_caller.PipelineCaller('Vowelizer', KELIME, os.environ['pipeline_token'], 'word')
 
-        try:
+        r = re.compile(r'(.+?\n)', re.MULTILINE)
 
-            caller = pipeline_caller.PipelineCaller('Vowelizer', KELIME, os.environ['pipeline_token'], 'word')
+        response = caller.call()
 
-            r = re.compile(r'(.+?\n)', re.MULTILINE)
-
-            response = caller.call()
-
-            print(response)
-            assert len(re.findall(r, response)) == 4
-        except:
-            self.fail('Exception thrown')
+        print(response)
+        assert len(re.findall(r, response)) == 4
 
     def module_pipelineNoisy_sentence_test(self):
+        caller = pipeline_caller.PipelineCaller('pipelineNoisy', UCDORT, os.environ['pipeline_token'], 'sentence')
 
-        try:
+        r1 = re.compile(r'(1)(\t.+?){7,}', re.MULTILINE)
+        r2 = re.compile(r'(5)(\t.+?){7,}', re.MULTILINE)
 
-            caller = pipeline_caller.PipelineCaller('pipelineNoisy', UCDORT, os.environ['pipeline_token'], 'sentence')
+        response = caller.call()
 
-            r1 = re.compile(r'(1)(\t.+?){7,}', re.MULTILINE)
-            r2 = re.compile(r'(5)(\t.+?){7,}', re.MULTILINE)
-
-            response = caller.call()
-
-            print(response)
-            assert len(re.findall(r1, response)) == 2 and len(re.findall(r2, response)) == 1
-        except:
-            self.fail('Exception thrown')
+        print(response)
+        assert len(re.findall(r1, response)) == 2 and len(re.findall(r2, response)) == 1
 
     def module_pipelineNoisy_whole_test(self):
+        caller = pipeline_caller.PipelineCaller('pipelineNoisy', KATANA, os.environ['pipeline_token'], 'whole')
 
-        try:
+        r = re.compile(r'(\d+)(\t.+?){7,}', re.MULTILINE)
 
-            caller = pipeline_caller.PipelineCaller('pipelineNoisy', KATANA, os.environ['pipeline_token'], 'whole')
+        response = caller.call()
 
-            r = re.compile(r'(\d+)(\t.+?){7,}', re.MULTILINE)
-
-            response = caller.call()
-
-            print(response)
-            assert len(re.findall(r, response)) == 29
-        except:
-            self.fail('Exception thrown')
+        print(response)
+        assert len(re.findall(r, response)) == 29
 
     def tool_exception_test(self):
-        try:
-            exec (open('./pipeline_caller.py').read())
-        except:
-            self.fail('Exception thrown')
+        exec (open('./pipeline_caller.py').read())
 
     def tool_noisy_test(self):
-
-        try:
-            #subprocess.Popen("python3 pipeline_caller.py katana.txt")
-            script_path = "./pipeline_caller.py"
-            subprocess.check_call([sys.executable or 'python3', script_path, "test_inputs/katana.txt"],
+        #subprocess.Popen("python3 pipeline_caller.py katana.txt")
+        script_path = "./pipeline_caller.py"
+        subprocess.check_call([sys.executable or 'python3', script_path, "test_inputs/katana.txt"],
                        cwd=os.path.dirname(script_path))
-        except:
-            print ("Unexpected error:", sys.exc_info()[0])
-            self.fail('Exception thrown')
 
 
 if __name__ == '__main__':
